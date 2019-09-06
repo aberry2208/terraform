de1de8cd79e866d187085d4a61679648a7176b4c
aberry2208 <34184404+aberry2208@users.noreply.github.com>
2019-09-06 13:24:44 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,44 +1,76 @@
-/***********************************************
-  Variable Confiugruation File
-***********************************************/
 variable "project" {
   default = "regal-scholar-235117"
 }
 
+variable "env" {
+  default = "clgx"
+}
+
+variable "name" {
+  default = "cl-dev-mta-test-1a"
+}
+
+variable "instance_type" {
+  default = "f1-micro" // Update to correct instance type
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
 variable "region" {
-  default = "us-east1"
+  default = "us-central1" // Can be changed to what ever region we are working with
+}
+
+variable "disk_image_project" {
+  default = "centos-cloud" // Update to image factory location //
+}
+
+variable "disk_image_family" {
+  default = "centos-7" // Update to correct image //
+}
+
+variable "environment" {
+  default = "prd"
+}
+
+variable "designation" {
+  default = "1"
+}
+
+variable "purpose"{
+  default = "matrix-mail"
 }
 
 variable "number_of_relay" {
-  default = "3"
+  default = "2"
 }
 
 variable "number_of_mta" {
-  default = "3"
+  default = "2"
 }
 
-variable "tags" {
-  default = []
-  type    = "list"
+variable "organization" {
+  default = "tac-systems"
 }
 
-variable "primary_company_code" {
-  default = "2870"
+variable "application" {
+  default = "matrix"
+}
+
+variable "tier" {
+  default = "app"
 }
 
 variable "chargeback_cost_center" {
-  default = "2870"
-}
-
-variable "primary_cost_center" {
-  default = "758371"
+  default = "123456789"
 }
 
 variable "case_wise_appid" {
-  default = "16f9a24583b111e390b390b5005056850489"
+  default = "123456789wert"
 }
 
-variable "financial_owner" {
+variable  "financial_owner" {
   default = "aberry"
 }
 
@@ -47,51 +79,67 @@ variable "tech_lead" {
 }
 
 variable "resolver_group" {
-  default = "aberry"
+  default = "IT"
 }
 
-variable "tier" {
-  default = "app"
+variable "primary_company_code" {
+  default = "1234"
 }
 
-variable "purpose" {
-  default = "matrix-mail"
+variable "primary_cost_center" {
+  default = "1234"
 }
 
-variable "application" {
-  default = "matrix"
+variable "chargeback_company_code" {
+  default = "7894556"
 }
 
-variable "organization" {
-  default = "tac"
+variable "machine_type" {
+  default = "f1-micro"
 }
 
-variable "labels" {
-  default = "maps"
-}
-variable "disk_type" {
-  default = "pd-ssd"
+variable "disk_size" {
+  default = "10"
 }
 
 variable "disk_type" {
   default = "pd-ssd"
+}
+
+variable "subnetwork" {
+  default = "default"
+}
+
+variable "subnetwork_project" {
+  default = "regal-scholar-235117"
+}
+
+variable "service_account" {
+  default = ""
+}
+
+variable "service_account_scope" {
+  default = ["https://www.googleapis.com/auth/cloud-platform"]
+  type    = "list"
+}
+
+variable "tags" {
+  default = []
+  type    = "list"
+}
+
+variable "metadata" {
+  type    = "map"
+  default = {}
 }
 
 variable "additional_relay_disk_sizes" {
   type    = "list"
-  default = ["2","50"]
+  default = ["10"]
 }
 
 variable "additional_mta_disk_sizes" {
   type    = "list"
-  default = ["2","50"]
-}
-
-variable "company" {
-  default = "TAC"
-}
-
-variable "env" {
-  default = "dev"
+  default = ["2", "10"]
 }
 
